cf27125bab59fbabcb865b766bc474c223d489d4
wufuliang <1>
2020-11-12 15:10:21 +0800

    创建几个单页面

---
+++ src/pages/StaffManage/StaffManage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item> <i class="el-icon-lx-calendar"></i> 员工管理 </el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="container">
+            <h4>该板块供需请提供，有需要请致电：028-86944045</h4>
+        </div>
+    </div>
+</template>
