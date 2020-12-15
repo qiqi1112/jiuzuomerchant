139d389e0c9112c4101d653fd43d5b02296a7830
liqi <1904710955@qq.com>
2020-12-15 14:38:45 +0800

    ''

--- src/pages/Begintabledetails/ktv.vue
+++ src/pages/Begintabledetails/ktv.vue
@@ -1,8 +1,5 @@
 <template>
     <div class="error-page">
-        <template>
-            <el-table v-loading="loading" style="width: 100%"> </el-table>
-        </template>
     </div>
 </template>
 
@@ -13,12 +10,12 @@
 export default {
     data() {
         return {
-            loading: true
+           
         };
     },
     methods: {
         open() {
-            this.$alert('KTV不具备此功能', '标题名称', {
+            this.$alert('KTV不具备此功能', '提示：', {
                 confirmButtonText: '确定',
                 callback: (action) => {
                     this.$router.go(-1);
@@ -27,10 +24,7 @@ export default {
         }
     },
     created() {
-        setTimeout(() => {
-            this.loading = false; 
-            this.open();
-        }, 1000);
+        this.open();
     }
 };
 </script>
