6fa6ddb5c6363de6d233d082cf53c34702bf1c93
wufuliang <1>
2021-02-23 16:41:23 +0800

    去掉登录输入框输入的首尾空格

--- src/components/page/Login.vue
+++ src/components/page/Login.vue
@@ -4,12 +4,12 @@
             <div class="ms-title">玖座商家管理系统</div>
             <el-form :model="param" :rules="rules" ref="login" label-width="0px" class="ms-content">
                 <el-form-item prop="username">
-                    <el-input v-model="param.username" placeholder="用户名" @keyup.enter.native="submitForm">
+                    <el-input v-model.trim="param.username" placeholder="用户名" @keyup.enter.native="submitForm">
                         <el-button slot="prepend" icon="el-icon-lx-people"></el-button>
                     </el-input>
                 </el-form-item>
                 <el-form-item prop="password">
-                    <el-input type="password" placeholder="密码" v-model="param.password" @keyup.enter.native="submitForm">
+                    <el-input type="password" placeholder="密码" v-model.trim="param.password" @keyup.enter.native="submitForm">
                         <el-button slot="prepend" icon="el-icon-lx-lock"></el-button>
                     </el-input>
                 </el-form-item>
